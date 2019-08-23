d0479c75184a1f7af55026515ee5f8e726aab09d
Joseph Newman <paradoxbuilder@gmail.com>
2019-08-23 15:05:59 -0600

    Added some documentation to the collider class.

--- Collider.cs
+++ Collider.cs
@@ -1,7 +1,17 @@
 ﻿namespace out_and_back
 {
+    /// <summary>
+    /// Utility class to check if two entities collide.
+    /// </summary>
     class Collider
     {
+        /// <summary>
+        /// Checks if any two entities collide, and calls their respective
+        /// collision functions.
+        /// </summary>
+        /// <param name="a">Entity a</param>
+        /// <param name="b">Entity b</param>
+        /// <returns>True if they do collide.</returns>
         public static bool DoCollide(Entity a, Entity b)
         {
             if (a == null || b == null) return false;
