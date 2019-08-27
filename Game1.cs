f90377148a93755f263f6566f15b9dd919fbc233
Joseph Newman <paradoxbuilder@gmail.com>
2019-08-27 11:53:48 -0600

    Allowed for levels to be ignored when starting the game.

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Xna.Framework;
+﻿#define RUN_LEVEL
+
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+
 
 namespace out_and_back
 {
@@ -13,7 +16,9 @@ namespace out_and_back
         internal SpriteBatch spriteBatch;
 
         internal EnitityManager Entities;
+#if RUN_LEVEL
         Level level;
+#endif
         Player player;
 
         public Game1()
@@ -34,7 +39,9 @@ namespace out_and_back
         {
             Entity.DefaultRemovalEvent = EntityRemoved;
             player = new Player(this, 0, new Vector2(250, 250));
+#if RUN_LEVEL
             level = Level.Level1(this);
+#endif
             base.Initialize();
         }
 
@@ -85,8 +92,9 @@ namespace out_and_back
                 Exit();
 
             // TODO: Add your update logic here
+#if RUN_LEVEL
             level.Update(gameTime);
-
+#endif
             base.Update(gameTime);
         }
 
@@ -100,7 +108,9 @@ namespace out_and_back
 
             // TODO: Add your drawing code here
             spriteBatch.Begin();
+#if RUN_LEVEL
             level.Draw(gameTime);
+#endif
             base.Draw(gameTime);
             spriteBatch.End();
         }
